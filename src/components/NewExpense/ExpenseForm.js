acf0dca640312aef124b370846a115fcc39179cd
Aleksandr <alex.avramenko2004@gmail.com>
2022-12-15 13:26:50 -0500

    Handling user inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,6 +8,7 @@ const ExpenseForm = () => {
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+        
     }
     const AmountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
@@ -16,20 +17,37 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const ExpenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+}
+
+    
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
                     <input 
                         type="text" 
+                        value={enteredTitle}
                         onChange={titleChangeHandler}
                     />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
                     <input 
-                        type="number" 
+                        type="number"
+                        value={enteredAmount} 
                         min="0.01" 
                         step='0.01' 
                         onChange={AmountChangeHandler}
@@ -38,7 +56,8 @@ const ExpenseForm = () => {
                 <div className="new-expense__control">
                     <label>Date</label>
                     <input 
-                        type="date" 
+                        type="date"
+                        value={enteredDate} 
                         min='2019-01-01' 
                         max='2022-12-31' 
                         onChange={DateChangeHandler}
